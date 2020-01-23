318976d2084c1ec5e4b5d65dfbab77b9caa18872
Optimalgitit <jggsecondary@gmail.com>
2020-01-23 21:39:02 +0400

    Hide password input

--- encrypt.py
+++ encrypt.py
@@ -1,7 +1,9 @@
 from EncryptionWare import EncryptionWare
+from getpass import getpass
+
 
 TO_ENCRYPT = input("Please enter the text you would like to encrypt: ")
-PASS = input("Enter Password: ")
+PASS = getpass()
 
 Encrypt = EncryptionWare(TO_ENCRYPT, PASS)
 Encrypt.encrypt_verbose()
