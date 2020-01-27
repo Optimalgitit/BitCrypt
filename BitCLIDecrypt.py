5bd786cdd77e2c5ddb62ea96ee45201044ff4877
OPtimal <jggsecondary@gmail.com>
2020-01-27 12:37:21 +0400

    Add automatic clipboard copy

--- BitCLIDecrypt.py
+++ BitCLIDecrypt.py
@@ -1,8 +1,10 @@
 from BitLib import BitLib
 from getpass import getpass
+import pyperclip as pyclip
 
 TO_ENCRYPT = input("Please enter the text you would like to decrypt: ")
 PASS = getpass(prompt="Enter Password: ")
 
 Encrypt = BitLib(TO_ENCRYPT, PASS)
-Encrypt.decrypt_verbose()
+Encrypted = Encrypt.decrypt_verbose()
+pyclip.copy(Encrypted)
--- BitCLIEncrypt.py
+++ BitCLIEncrypt.py
@@ -1,9 +1,10 @@
 from BitLib import BitLib
 from getpass import getpass
-
+import pyperclip as pyclip
 
 TO_ENCRYPT = input("Please enter the text you would like to encrypt: ")
 PASS = getpass(prompt="Enter Password: ")
 
 Encrypt = BitLib(TO_ENCRYPT, PASS)
-Encrypt.encrypt_verbose()
+Encrypted = Encrypt.encrypt_verbose()
+pyclip.copy(Encrypted)
